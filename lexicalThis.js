3e5677bc25e0d867517cc56384f9ace82391287c
StepanidaKirillina1 <steshakirillina@yandex.ru>
2026-04-03 20:26:54 +0900

    add solution for "Lexical this"

---
+++ lexicalThis.js
@@ -0,0 +1,24 @@
+/*
+
+Description:
+
+https://www.codewars.com/kata/55a13556ca4a6d0ab4000003
+
+Lexical this
+Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+Output
+Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+
+*/
+
+const Person = function () {
+    const person = {
+        _name: "Leroy",
+        _friends: [],
+        fillFriends(f) {
+            f.forEach(friend => this._friends.push(friend));
+        }
+    }
+
+    return person;
+}
